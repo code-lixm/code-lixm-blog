d397e1bf4ea06ed9ea30c2c386919f3a3ac04a0b
lixiaoming-bit <1404241558@qq.com>
2022-10-17 21:15:21 +0800

    初始化博客

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { $notion } = useNuxtApp()
+const { data } = await useAsyncData("notion-index", () => $notion.getPageBlocks("6613c65443bc48d29445e7ef9fd066f2"))
+const { mapPageUrl, pageLinkOptions } = useProps()
+</script>
+
+<template>
+  <div>
+    <NotionRenderer :blockMap="data" fullPage prism katex :mapPageUrl="mapPageUrl" :pageLinkOptions="pageLinkOptions" />
+  </div>
+</template>
