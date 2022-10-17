e930c697351cc40defa4bfc0f44571cdb065b402
lixiaoming-bit <1404241558@qq.com>
2022-10-17 23:00:57 +0800

    refactor: 修改标题

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt"
 
 export default defineNuxtConfig({
   meta: {
-    title: "Nuxt3 Notion",
+    title: "joycewil blog",
     meta: [{ charset: "utf-8" }, { name: "viewport", content: "width=device-width, initial-scale=1" }],
     link: [{ rel: "icon", type: "image/png", href: "/logo.png" }],
   },
